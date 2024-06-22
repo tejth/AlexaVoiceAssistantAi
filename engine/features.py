369eafafcf2fe8bc1a8ba2c45d66aa4d21b81406
tejth <ptejendra91@gmail.com>
2024-06-22 10:46:22 +0530

    ADD CLICK EVENT TO MIC BUTTON

--- engine/features.py
+++ engine/features.py
@@ -1,7 +1,8 @@
 from playsound import playsound
-
+import eel
 
 #playing assistant sound function
+@eel.expose
 def playAssistantSound():
     music_dir = "C:\\Users\\tejth\\Desktop\\Jarvis\\www\\assets\\audio\\start_sound.mp3"
     playsound(music_dir)
--- www/main.js
+++ www/main.js
@@ -34,4 +34,12 @@ $(document).ready(function () {
       sync: true,
     },
   });
+
+  // mic button click event
+  $("#MicBtn").click(function () {
+    eel.playAssistantSound();
+    $("#Oval").attr("hidden", true);
+    $("#SiriWave").attr("hidden", false);
+    eel.allCommands()();
+  });
 });
