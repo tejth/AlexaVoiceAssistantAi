b628e733521363c1ef1824bf186fc71b0cb39270
tejth <ptejendra91@gmail.com>
2024-06-22 11:18:22 +0530

    add convert text to speech functionality using speech recognition and pyaudio library

--- engine/command.py
+++ engine/command.py
@@ -1,5 +1,5 @@
 import pyttsx3
-
+import speech_recognition as sr
 
 
 def speak(text):
@@ -9,4 +9,28 @@ def speak(text):
     engine.setProperty('rate', 174)
     engine.say(text)
     engine.runAndWait()
-speak(" india is my country! , all indians are my brothers and sisters")
+
+
+def takecommand():
+
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print('listening....')
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+        
+        audio = r.listen(source, 10, 6)
+
+    try:
+        print('recognizing')
+        query = r.recognize_google(audio, language='en-in')
+        print(f"user said: {query}")
+       
+    except Exception as e:
+        return ""
+    
+    return query.lower()
+
+text = takecommand()
+speak(text)
