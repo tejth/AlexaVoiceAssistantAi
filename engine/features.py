8f9ee77951f1576b76afab908a5f4f0ca99fc49d
tejth <ptejendra91@gmail.com>
2024-06-22 10:22:43 +0530

    create input box , add siri wave ,add playsound module

---
+++ engine/features.py
@@ -0,0 +1,8 @@
+from playsound import playsound
+
+
+#playing assistant sound function
+def playAssistantSound():
+    music_dir = "C:\\Users\\tejth\\Desktop\\Jarvis\\www\\assets\\audio\\start_sound.mp3"
+    playsound(music_dir)
+    
