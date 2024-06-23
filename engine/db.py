adf301248dbf0c860ec4909c4f1dbf6020d9ae3d
tejth <ptejendra91@gmail.com>
2024-06-23 16:35:45 +0530

    make database data work by speech recognition  by modifying opencommand function inside features.js and using select sql query
    webbrowser module is used to open websites

--- engine/db.py
+++ engine/db.py
@@ -16,15 +16,82 @@ cursor = con.cursor()
 # query = "INSERT INTO sys_command VALUES (null,'vs code', 'C:\\Users\\tejth\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe')"
 # cursor.execute(query)
 
-# query = "INSERT INTO sys_command VALUES (null,'eDEX-UI', 'C:\\Users\\tejth\\AppData\\Local\\Programs\\eDEX-UI\\eDEX-UI.exe')"
+# query = "INSERT INTO sys_command VALUES (null,'Microsoftedge', 'C:\Program Files (x86)\Microsoft\Edge\Application')"
 # cursor.execute(query)
 
 
-query = "INSERT INTO web_command VALUES (null,'youtube', 'https://www.youtube.com/')"
-cursor.execute(query)
+
+# query = """
+#     UPDATE web_command
+#     SET url = 'https://www.canva.com/'
+#     WHERE name = 'canva' AND url = 'https://www.canva.com//'
+# """
+# cursor.execute(query)
 
 
-query = "INSERT INTO web_command VALUES (null,'canva', 'https://www.canva.com//')"
-cursor.execute(query)
+# query = "INSERT INTO web_command VALUES (null,'youtube', 'https://www.youtube.com/')"
+# cursor.execute(query)
+
+
+# query = "INSERT INTO web_command VALUES (null,'canva', 'https://www.canva.com/')"
+# cursor.execute(query)
+
+# query = "INSERT INTO web_command VALUES (null,'geek for geeks', 'https://www.geeksforgeeks.org/')"
+# cursor.execute(query)
+
+# query = "INSERT INTO web_command VALUES (null,'github', 'https://github.com/')"
+# cursor.execute(query)
+
+# query = "INSERT INTO web_command VALUES (null,'mdn', 'https://developer.mozilla.org/en-US/')"
+# cursor.execute(query)
+
+# query = "INSERT INTO web_command VALUES (null,'chatgpt', 'https://chatgpt.com/')"
+# cursor.execute(query)
+
+# query = "INSERT INTO web_command VALUES (null,'leetcode', 'https://leetcode.com/')"
+# cursor.execute(query)
+
+
+# query = "INSERT INTO web_command VALUES (null,'w3schools', 'https://www.w3schools.com/')"
+# cursor.execute(query)
+
+# query = "INSERT INTO web_command VALUES (null,'instagram', 'https://www.instagram.com/')"
+# cursor.execute(query)
+
+# query = "INSERT INTO web_command VALUES (null,'facebook', 'https://www.facebook.com/')"
+# cursor.execute(query)
+
+# query = "INSERT INTO web_command VALUES (null,'gmail', 'https://www.gmail.com/')"
+# cursor.execute(query)
+
+# query = "INSERT INTO web_command VALUES (null,'googledrive', 'https://drive.google.com/')"
+# cursor.execute(query)
+
+# query = "INSERT INTO web_command VALUES (null,'bootstrap', 'https://getbootstrap.com/')"
+# cursor.execute(query)
+
+# query = "INSERT INTO web_command VALUES (null,'ilovepdf', 'https://www.ilovepdf.com/')"
+# cursor.execute(query)
+
+# query = "INSERT INTO web_command VALUES (null,'flipkart', 'https://www.flipkart.com/')"
+# cursor.execute(query)
+
+# query = "INSERT INTO web_command VALUES (null,'amazon', 'https://www.amazon.in/')"
+# cursor.execute(query)
+
+# query = "INSERT INTO web_command VALUES (null,'google', 'https://google.com/')"
+# cursor.execute(query)
+
+
+# testing module
+# app_name = "android studio"
+# cursor.execute('SELECT path FROM sys_command WHERE name IN (?)', (app_name,))
+# results = cursor.fetchall()
+# print(results[0][0])
+
+
+
+
+
 
 con.commit()
--- engine/features.py
+++ engine/features.py
@@ -7,6 +7,13 @@ from engine.command import speak
 import os
 
 import pywhatkit as kit
+import webbrowser
+import sqlite3
+
+con = sqlite3.connect("eco.db")
+cursor = con.cursor()
+
+
 
 #playing assistant sound function
 @eel.expose
@@ -19,12 +26,37 @@ def openCommand(query):
     query = query.replace(ASSISTANT_NAME, "")
     query = query.replace("open", "")
     query.lower()
-    
-    if query!="":
-        speak("Opening "+query)
-        os.system("start "+query)
-    else:
-        speak("not found")
+
+    app_name = query.strip()
+
+    if app_name != "":
+
+        try:
+            cursor.execute(
+                'SELECT path FROM sys_command WHERE name IN (?)', (app_name,))
+            results = cursor.fetchall()
+
+            if len(results) != 0:
+                speak("Opening "+query)
+                os.startfile(results[0][0])
+
+            elif len(results) == 0: 
+                cursor.execute(
+                'SELECT url FROM web_command WHERE name IN (?)', (app_name,))
+                results = cursor.fetchall()
+                
+                if len(results) != 0:
+                    speak("Opening "+query)
+                    webbrowser.open(results[0][0])
+
+                else:
+                    speak("Opening "+query)
+                    try:
+                        os.system('start '+query)
+                    except:
+                        speak("not found")
+        except:
+            speak("some thing went wrong")
         
         
         
